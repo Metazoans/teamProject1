1c5f90fb18778786d52af1b07157085452817209
admin <admin@DESKTOP-1QPVEE5>
2024-11-05 17:54:07 +0900

    241105-1753

--- teamProject1/src/main/webapp/js/chat.js
+++ teamProject1/src/main/webapp/js/chat.js
@@ -1,3 +1,25 @@
 /**
  * chat.js
  */
+
+document.querySelector('#seller').addEventListener('click', function(e) {
+	let toId = document.querySelector('#seller').innerHTML
+	$.ajax('chatList.do?fromId=' + fromId + '&toId=' + toId)
+		.done(function(result) {
+			console.log(result)
+			result.forEach((item) => {
+				if (item.fromId == fromId) {
+					$('<p/>').append($('<div/>').addClass('floatRight chatting').text(item.message))
+						.appendTo($('.modal-chat'))
+				} else {
+					$('<p/>').append($('<div/>').addClass('floatLeft chatting').text(item.message))
+						.appendTo($('.modal-chat'))
+				}
+			});
+		})
+		.fail(function(err) {
+			console.log(err);
+		})
+
+
+})

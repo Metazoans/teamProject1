dc14d0088be63dea9912ae28449bbc758ccb497b
admin <admin@DESKTOP-5FFM27G>
2024-11-08 12:04:53 +0900

    241108-1204

--- teamProject1/src/test/java/com/yedam/test/AppTest.java
+++ teamProject1/src/test/java/com/yedam/test/AppTest.java
@@ -1,6 +1,8 @@
 package com.yedam.test;
 
-import com.yedam.common.PageDTO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.yedam.common.SearchDTO;
 import com.yedam.service.item.ItemService;
 import com.yedam.service.item.ItemServiceImpl;
 
@@ -8,12 +10,15 @@ public class AppTest {
 
 	public static void main(String[] args) {
 
-		
+		SearchDTO search = new SearchDTO();
+		search.setPage(1);
+		search.setKeyword("test5");
+
 		ItemService svc = new ItemServiceImpl();
-		
-		
-		PageDTO dto = new PageDTO(1, svc.totalSell("test1"));
-		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+		System.out.println(gson.toJson(svc.sellListPage(search)));
+
 	}
-	
+
 }
